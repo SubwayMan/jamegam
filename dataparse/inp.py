4dcb6c5e90b331daaa07d52c5ed5aa72536e2dca
SubwayMan <tyche4490@gmail.com>
2021-03-15 11:36:16 -0700

    added parsing feature

--- dataparse/inp.py
+++ dataparse/inp.py
@@ -5,13 +5,10 @@ oname = sys.argv[2]
 dest = os.path.join(os.getcwd(), "..", "source", oname)
 
 dat = open(fname, "r").read()
-dat = ":{\n".join(dat.split(":{"))
-dat = ":[\n".join(dat.split(":["))
-dat = "\n".join(dat.split("\\n"))
-dat = "},\n".join(dat.split("},"))
-dat = "],\n".join(dat.split("],"))
-dat = "\n}".join(dat.split("}"))
-dat = "\n]".join(dat.split("]"))
+a1 = [":{\n", ":[\n", "\n", "},\n", "],\n", "\n}", "\n]"]
+a2 = [":{", ":[", "\\n", "},", "],", "}", "]"]
+for i in range(len(a1)):
+    dat = a1[i].join(dat.split(a2[i]))
 
 k = open(dest, "w")
 k.write(dat)
---
+++ dataparse/out.py
@@ -0,0 +1,15 @@
+import sys
+import os
+orig = os.path.join(os.getcwd(), "..", "source", sys.argv[1])
+out = sys.argv[2]
+
+a1 = [":{\n", ":[\n", "},\n", "],\n", "\n}", "\n]", "\n"]
+a2 = [":{", ":[", "},", "],", "}", "]", "\\n"]
+
+dat = open(orig, "r").read()
+for i in range(len(a1)):
+    dat = a2[i].join(dat.split(a1[i]))
+
+k = open(out, "w")
+k.write(dat)
+k.close()
